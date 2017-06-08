3dbe542f9c3f5a797b9859ba64d754633d5acae2
Matt Warman <warmanmr@gmail.com>
2017-06-08 13:04:11 -0400

    Added slf4j logging support.

--- src/main/java/com/leanstacks/hello/lambda/GreetingRequestHandler.java
+++ src/main/java/com/leanstacks/hello/lambda/GreetingRequestHandler.java
@@ -1,25 +1,28 @@
 package com.leanstacks.hello.lambda;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.leanstacks.hello.lambda.model.Greeting;
 import com.leanstacks.hello.lambda.model.User;
 
 public class GreetingRequestHandler implements RequestHandler<User, Greeting> {
 
+    private static final Logger logger = LoggerFactory.getLogger(GreetingRequestHandler.class);
+
     @Override
     public Greeting handleRequest(User user, Context context) {
-        LambdaLogger logger = context.getLogger();
-        logger.log("> handleRequest\n");
-        logger.log("  user.name: " + user.getName() + "\n");
+        logger.info("> handleRequest");
+        logger.debug("  user.name: {}", user.getName());
 
         String greetingText = String.format("Hello %1s!", user.getName());
 
         Greeting greeting = new Greeting(greetingText);
-        logger.log("  greeting.text: " + greeting.getText() + "\n");
+        logger.debug("  greeting.text: {}", greeting.getText());
 
-        logger.log("< handleRequest\n");
+        logger.info("< handleRequest");
         return greeting;
     }
 
