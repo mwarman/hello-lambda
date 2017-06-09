c69533e398badb38737a86f2a32db1460c114fbf
Matt Warman <warmanmr@gmail.com>
2017-06-09 13:15:19 -0400

    Docs

--- src/main/java/com/leanstacks/hello/lambda/service/GreetingServiceBean.java
+++ src/main/java/com/leanstacks/hello/lambda/service/GreetingServiceBean.java
@@ -9,20 +9,45 @@ import org.springframework.stereotype.Service;
 import com.leanstacks.hello.lambda.model.Greeting;
 import com.leanstacks.hello.lambda.model.User;
 
+/**
+ * The GreetingServiceBean implements the behaviors of the {@link GreetingService}.
+ * 
+ * @author Matt Warman
+ *
+ */
 @Service
 public class GreetingServiceBean implements GreetingService {
 
+    /**
+     * The Logger for this class.
+     */
     private static final Logger logger = LoggerFactory.getLogger(GreetingServiceBean.class);
 
+    /**
+     * A String template for formatting Greeting text.
+     */
     private static final String GREETING_FORMAT = "%1s %2s!";
 
+    /**
+     * The Spring Environment.
+     */
     private Environment env;
 
+    /**
+     * Construct a GreetingServiceBean with the supplied parameters.
+     * 
+     * @param env An Environment object. Spring's Environment wrapper.
+     */
     @Autowired
     public GreetingServiceBean(Environment env) {
         this.env = env;
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.leanstacks.hello.lambda.service.GreetingService#create(com.leanstacks.hello.lambda.model.User)
+     */
     @Override
     public Greeting create(final User user) {
         logger.info("> create");
