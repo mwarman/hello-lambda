3b0d4e5fc39785881feff99b0b724f21e00baa32
Matt Warman <warmanmr@gmail.com>
2017-06-09 09:45:52 -0400

    Environment support via Springs Environment class.

--- src/main/java/com/leanstacks/hello/lambda/service/GreetingServiceBean.java
+++ src/main/java/com/leanstacks/hello/lambda/service/GreetingServiceBean.java
@@ -2,6 +2,8 @@ package com.leanstacks.hello.lambda.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import com.leanstacks.hello.lambda.model.Greeting;
@@ -14,11 +16,19 @@ public class GreetingServiceBean implements GreetingService {
 
     private static final String GREETING_FORMAT = "%1s %2s!";
 
+    private Environment env;
+
+    @Autowired
+    public GreetingServiceBean(Environment env) {
+        this.env = env;
+    }
+
     @Override
     public Greeting create(final User user) {
         logger.info("> create");
 
-        final String greetingText = String.format(GREETING_FORMAT, "Hello", user.getName());
+        final String greetingText = String.format(GREETING_FORMAT, env.getProperty("GREETING_PREFIX", "Hello"),
+            user.getName());
 
         final Greeting greeting = new Greeting(greetingText);
 
